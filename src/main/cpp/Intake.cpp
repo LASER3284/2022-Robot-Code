3222d6732bd764ad19300d6d5e927fb841a4ba3b
VLPatton <caes55.patton@gmail.com>
2022-01-22 09:25:37 -0600

    Begin Intake.cpp

---
+++ src/main/cpp/Intake.cpp
@@ -0,0 +1,28 @@
+/******************************************************************************
+	Description:	Implements the CIntake control class.
+	Classes:		CIntake
+	Project:		2022 Rapid React Robot Code
+******************************************************************************/
+
+#include "Intake.h"
+///////////////////////////////////////////////////////////////////////////////
+
+/******************************************************************************
+	Description:	CIntake constructor, init local variables/classes
+	Arguments:		None
+	Derived from:	Nothing
+******************************************************************************/
+CIntake::CIntake()
+{
+
+}
+
+/******************************************************************************
+	Description:	CIntake destructor, delete local class pointers
+	Arguments:		None
+	Derived from:	Nothing
+******************************************************************************/
+CIntake::~CIntake()
+{
+
+}
